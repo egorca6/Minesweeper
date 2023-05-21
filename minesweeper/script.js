58090f8a2aecdd312eac5ec63c410b50d46a1684
Yahor Tsytsarkin <egormail88@gmail.com>
2023-05-21 21:06:30 +0300

    feat: finished the task

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,5 +1,20 @@
-const body = document.querySelector('body');
+let body = document.querySelector('body');
 const main = document.createElement('main');
+const saveButton = document.createElement('button');
+saveButton.classList.add('save-button');
+saveButton.textContent = 'Save';
+main.append(saveButton);
+const loadButton = document.createElement('button');
+loadButton.classList.add('save-button');
+loadButton.textContent = 'Load';
+main.append(loadButton);
+const statsButton = document.createElement('button');
+statsButton.classList.add('save-button');
+statsButton.textContent = 'Stats';
+main.append(statsButton);
+const result = document.createElement('div');
+result.classList.add('result-hide');
+main.append(result);
 main.classList.add('main');
 body.append(main);
 const title = document.createElement('h1');
@@ -12,12 +27,18 @@ const myCliks = document.createElement('div');
 myCliks.classList.add('myCliks');
 myCliks.innerText = 'Всего кликов = ';
 navbar.append(myCliks);
-const level = document.createElement('div');
+const levelDiv = document.createElement('div');
+levelDiv.classList.add('level-div');
+navbar.append(levelDiv);
+const level = document.createElement('button');
 level.classList.add('level');
-navbar.append(level);
-const minesNumber = document.createElement('div');
+levelDiv.append(level);
+const minesNumber = document.createElement('input');
 minesNumber.classList.add('mines-number');
-navbar.append(minesNumber);
+levelDiv.append(minesNumber);
+const changeMines = document.createElement('button');
+changeMines.classList.add('change-mines');
+navbar.append(changeMines);
 const darkLight = document.createElement('button');
 darkLight.classList.add('dark-light');
 navbar.append(darkLight);
@@ -39,6 +60,9 @@ navbar.append(badClick);
 const wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
 main.append(wrapper);
+const footer = document.createElement('div');
+footer.classList.add('footer');
+main.append(footer);
 const wrapperTop = document.createElement('div');
 wrapperTop.classList.add('wrapper-top');
 wrapper.append(wrapperTop);
@@ -55,21 +79,20 @@ newGame.innerHTML = 'New Game';
 const time = document.createElement('div');
 time.classList.add('time');
 wrapperTop.append(time);
-const result = document.createElement('div');
-result.classList.add('result');
-wrapper.append(result);
-const results = [];
+let results = [];
 let gameOver = false;
+let gameState = {};
 
 function startGame(width, height, bombsCount) {
   const buttonCount = width * height;
   wrapperField.innerHTML = '<button class = "cell"> </button>'.repeat(buttonCount);
   let bombs = [...Array(buttonCount).keys()].sort(() => Math.random() - 0.5).slice(0, bombsCount);
-  const cells = [...wrapperField.children];
+  let cells = [...wrapperField.children];
   let closedCells = buttonCount;
   let flagsCount = bombsCount;
   mines.innerHTML = flagsCount;
   let clickCount = 0;
+  let buttons = wrapperField.querySelectorAll('button');
   const startTime = new Date();
   const x = setInterval(() => {
     const newTime = new Date();
@@ -123,14 +146,14 @@ function startGame(width, height, bombsCount) {
     result.innerHTML = '';
     myResult.forEach((res) => {
       const resultElement = document.createElement('div');
-      resultElement.textContent = res;
-      result.append(resultElement);
+        resultElement.textContent = res;
+        result.append(resultElement);
+      wrapperField.removeEventListener('contextmenu', contextMenuEvent);
     });
   }
 
   function open(row, column) {
     if (!isValid(row, column)) return;
-
     const index = row * width + column;
     const cell = cells[index];
     const count = getCount(row, column);
@@ -189,6 +212,9 @@ function startGame(width, height, bombsCount) {
   function clickEvent(event) {
     if (gameOver || event.target.tagName !== 'BUTTON') return;
     if (event.target.tagName === 'BUTTON') {
+      // console.log('firstClick начало - ', firstClick);
+      // console.log('height, width', height, width);
+      cells = [...wrapperField.children];
       const index = cells.indexOf(event.target);
       const column = index % width;
       const row = Math.floor(index / width);
@@ -197,18 +223,19 @@ function startGame(width, height, bombsCount) {
             - 0.5).slice(0, bombsCount);
       }
       firstClick = false;
-      // console.log(bombs);
+      // console.log('index, column, row', index, column, row);
+      // console.log('!isValid? =  ', !isValid(row, column));
       clickCount += 1;
       myCliks.innerText = clickCount;
       open(row, column);
     }
   }
 
-  wrapperField.addEventListener('click', clickEvent);
-
-  wrapperField.addEventListener('contextmenu', (event) => {
-    if (event.target.tagName === 'BUTTON') {
+  function contextMenuEvent(event) {
+    // console.log(event.target.tagName === 'BUTTON');
+    if (event && event.target && event.target.tagName === 'BUTTON') {
       event.preventDefault();
+      cells = [...wrapperField.children];
       const index = cells.indexOf(event.target);
       const cell = cells[index];
       cell.classList.toggle('flag');
@@ -222,50 +249,174 @@ function startGame(width, height, bombsCount) {
         cell.disabled = false;
       }
     }
-  });
+  }
+
+  wrapperField.addEventListener('contextmenu', contextMenuEvent);
 
   function music() {
     soundButton.classList.toggle('sound-stop');
-    console.log('contains ', soundButton.classList.contains('sound-stop'));
     if (soundButton.classList.contains('sound-stop')) {
       sound.volume = 0.25;
       sound.play();
-      console.log('pause =', sound.paused);
     } else {
       sound.pause();
-      console.log('играет ?');
     }
   }
 
-  soundButton.addEventListener('click', () => {
-    music();
-  });
+  soundButton.addEventListener('click', music);
 
   const newGameClick = () => {
     gameOver = false;
     wrapperField.removeEventListener('click', clickEvent);
     newGame.removeEventListener('click', newGameClick);
-    startGame(width, height, bombsCount);
+    wrapperField.removeEventListener('contextmenu', contextMenuEvent);
+    startGame(width, height, minesNumber.value);
     clearInterval(x);
     soundButton.addEventListener('click', () => {
       music();
+    });
+    statsButton.addEventListener('click', () => {
+      result.classList.toggle('result');
     });
   };
 
   newGame.addEventListener('click', newGameClick);
 
-  const buttons = wrapperField.querySelectorAll('button');
-
-  darkLight.addEventListener('click', () => {
-    body.classList.toggle('black');
+  function doDark() {
+    // body = document.querySelector('body');
+    // body.classList.toggle('black');
     buttons.forEach((button) => {
       button.classList.toggle('black');
     });
-    // buttons.classList.toggle('black');
-  });
+  }
 
-  minesNumber.innerText = bombsCount;
-  console.log('bombsCount', bombsCount);
+  darkLight.addEventListener('click', doDark);
+
+  minesNumber.value = bombsCount;
+
+  let levelGame = 0;
+
+  function setGameLevel() {
+    gameOver = false;
+    levelGame = (levelGame + 1) % 3;
+    switch (levelGame) {
+      case 0:
+        console.log('Легкий уровень игры');
+        level.className = 'level1';
+        break;
+      case 1:
+        level.className = 'level2';
+        console.log('Средний уровень игры');
+        break;
+      case 2:
+
+        console.log('Тяжелый уровень игры');
+        level.className = 'level3';
+        break;
+      default:
+        console.log('Некорректный уровень');
+    }
+    clearInterval(x);
+  }
+  // level.removeEventListener('click', setGameLevel);
+  level.addEventListener('click', setGameLevel);
+
+  function saveGame() {
+    gameState.field = wrapperField.innerHTML;
+    gameState.closedCells = closedCells;
+    gameState.flagsCount = flagsCount;
+    gameState.clickCount = clickCount;
+    gameState.time = time.innerHTML;
+    gameState.results = results;
+    gameState.bombs = bombs;
+    gameState.level = level.className;
+    gameState.wrapperField = wrapperField.className;
+    gameState.wrapper = wrapper.className;
+    gameState.width = width;
+    gameState.height = height;
+    gameState.minesNumber = minesNumber.value;
+    localStorage.setItem('gameState', JSON.stringify(gameState));
+  }
+
+  function loadGame() {
+    const savedState = localStorage.getItem('gameState');
+    if (savedState) {
+      darkLight.removeEventListener('click', doDark);
+      gameState = JSON.parse(savedState);
+      wrapperField.innerHTML = gameState.field;
+      closedCells = gameState.closedCells;
+      flagsCount = gameState.flagsCount;
+      clickCount = gameState.clickCount;
+      mines.innerHTML = flagsCount;
+      time.innerHTML = gameState.time;
+      results = gameState.results;
+      bombs = gameState.bombs;
+      level.className = gameState.level;
+      wrapperField.className = gameState.wrapperField;
+      wrapper.className = gameState.wrapper;
+      minesNumber.value = gameState.minesNumber;
+      width = gameState.width;
+      height = gameState.height;
+      firstClick = false;
+      wrapperField.addEventListener('click', clickEvent);
+      wrapperField.addEventListener('contextmenu', contextMenuEvent);
+      buttons = wrapperField.querySelectorAll('button');
+      darkLight.addEventListener('click', () => {
+        doDark();
+      });
+    }
+  }
+
+  saveButton.addEventListener('click', saveGame);
+  loadButton.addEventListener('click', loadGame);
+  wrapperField.addEventListener('click', clickEvent);
+
+  function confirmGameLevel() {
+    gameOver = false;
+    darkLight.removeEventListener('click', doDark);
+    wrapperField.removeEventListener('click', clickEvent);
+    wrapperField.removeEventListener('contextmenu', contextMenuEvent);
+    newGame.removeEventListener('click', newGameClick);
+    if (levelGame === 1) {
+      wrapperField.className = 'wrapper-field-medium';
+      wrapper.className = 'wrapper-medium';
+      startGame(15, 15, minesNumber.value);
+      buttons = wrapperField.querySelectorAll('button');
+      buttons.forEach((button) => {
+        button.classList.add('button-medium');
+      });
+    } else if (levelGame === 2) {
+      wrapperField.className = 'wrapper-field-hard';
+      wrapper.className = 'wrapper-hard';
+      startGame(25, 25, minesNumber.value);
+      buttons = wrapperField.querySelectorAll('button');
+      buttons.forEach((button) => {
+        button.classList.add('button-hard');
+      });
+    } else {
+      wrapperField.className = 'wrapper-field';
+      wrapper.className = 'wrapper';
+      startGame(10, 10, minesNumber.value);
+    }
+    clearInterval(x);
+    soundButton.addEventListener('click', () => {
+      music();
+    });
+    statsButton.addEventListener('click', () => {
+      result.classList.toggle('result');
+    });
+    wrapperField.addEventListener('contextmenu', () => {
+      contextMenuEvent();
+    });
+    changeMines.removeEventListener('click', confirmGameLevel);
+  }
+
+  changeMines.addEventListener('click', confirmGameLevel);
+
+  statsButton.addEventListener('click', () => {
+    result.classList.toggle('result');
+  });
 }
 
 startGame(10, 10, 10);
+// localStorage.clear();
